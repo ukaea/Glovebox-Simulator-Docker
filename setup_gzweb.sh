0b6b850d8ae21950f0826cbc40f63d5c1a7b05b8
Burroughes, Guy <guy.burroughes@ukaea.uk>
2020-08-20 13:30:03 +0100

    Bugfix: joint 1 is now stiffer

--- setup_gzweb.sh
+++ setup_gzweb.sh
@@ -8,7 +8,7 @@ git clone -b gzweb_1.4.0 https://github.com/osrf/gzweb.git
 source "/workspace/devel/setup.bash"
 source "/usr/share/gazebo/setup.sh"
 cd /root/gzweb
-./deploy.sh -m 
+./deploy.sh -m -t
 ls http/client/assets
 
 
